6483d7e80425cfe3a13cc342c6f392cd58fa319d
Raunak Kumar <raunakkumar634@gmail.com>
2026-02-23 16:51:34 +0530

    Time: 6 ms (96.67%), Space: 57 MB (89.14%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        
+        tail->next = l1 ? l1 : l2;
+        
+        return dummy.next;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+        
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        prev->next = nullptr;  // split list
+        
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(slow);
+        
+        return merge(l1, l2);
+    }
+};
