54411df1790a6838f2d3a06ae2d28a3845e47531
Raunak Kumar <raunakkumar634@gmail.com>
2026-02-24 10:52:51 +0530

    Time: 17 ms (75.36%), Space: 51.5 MB (69.56%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -19,3 +19,11 @@ class MyHashMap {
         map.remove(key);
     }
 }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
