658f567d8773c22b947cf6a12cfcb6eab633e7c1
Raunak Kumar <raunakkumar634@gmail.com>
2026-03-03 23:56:39 +0530

    Time: 27 ms (40.02%), Space: 59.3 MB (34.47%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        string s = "0";
+        
+        for(int i = 2; i <= n; i++) {
+            string temp = s;
+            
+            // invert the string
+            for(char &c : temp) {
+                c = (c == '0') ? '1' : '0';
+            }
+            
+            // reverse the inverted string
+            reverse(temp.begin(), temp.end());
+            
+            // build new string
+            s = s + "1" + temp;
+        }
+        
+        return s[k - 1];
+    }
+};
